2d47e941d0d38cd7e4ee3a98caa1e363f91024b0
henrypetrillo <henrypetrillo32@gmail.com>
2024-09-25 15:49:12 -0400

    Moved code from BlueJ to VSC
    Created this entire file inside of BlueJ without realizing that GitHub Desktop was not tracking the changes. Hopefully this issue is resolved for next lab.

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -1,32 +1,33 @@
 
 /**
  * The ClockDisplay class implements a digital clock display for a
- * European-style 24 hour clock. The clock shows hours and minutes. The 
- * range of the clock is 00:00 (midnight) to 23:59 (one minute before 
- * midnight).
+ * American-style 12 hour clock. The clock shows hours and minutes. Both AM and PM.
+ * Internally the clock runs as a 24hr clock then is converted accordingly when ran.
  * 
  * The clock display receives "ticks" (via the timeTick method) every minute
  * and reacts by incrementing the display. This is done in the usual clock
  * fashion: the hour increments when the minutes roll over to zero.
  * 
- * @author Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * 
+ * @author Henry Petrillo
+ * @version 2024.09.25
  */
 public class ClockDisplay
 {
     private NumberDisplay hours;
     private NumberDisplay minutes;
     private String displayString;    // simulates the actual display
+    private boolean isAM;
     
     /**
      * Constructor for ClockDisplay objects. This constructor 
-     * creates a new clock set at 00:00.
+     * creates a new clock set at 12:00am.
      */
     public ClockDisplay()
     {
         hours = new NumberDisplay(24);
         minutes = new NumberDisplay(60);
-        updateDisplay();
+        setTime(0,0); // 00:00 is the same as 12:00am
     }
 
     /**
@@ -78,7 +79,31 @@ public class ClockDisplay
      */
     private void updateDisplay()
     {
-        displayString = hours.getDisplayValue() + ":" + 
-                        minutes.getDisplayValue();
+        int currentHour = hours.getValue();
+        String amOrPmString;
+        
+        if (currentHour < 12) {
+            isAM = true;
+        } else {
+            isAM = false;
+        }
+        
+        if (currentHour == 0) {
+            currentHour = 12;
+        } else if (currentHour > 12) {
+            currentHour = currentHour - 12;
+        }
+        
+        if (currentHour == 12 && isAM) {
+            amOrPmString = "AM";
+        } else if (currentHour == 12 && !isAM) {
+            amOrPmString = "PM";
+        } else { // I'm too lazy to write if-else statement
+            //      when I can just use a ternary operator
+            //      since its a boolean
+            amOrPmString = isAM ? "AM" : "PM:";
+        }
+        
+        displayString = String.format("%02d:%02d %s", currentHour, minutes.getValue(), amOrPmString);
     }
 }
